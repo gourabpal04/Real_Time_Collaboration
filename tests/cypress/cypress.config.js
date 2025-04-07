3d3a22c685f2fb169a9965d15ca830fa21277d0f
Gourab Pal <110736631+gourabpal04@users.noreply.github.com>
2025-04-07 19:47:05 +0530

    Update cypress.config.js

--- tests/cypress/cypress.config.js
+++ tests/cypress/cypress.config.js
@@ -1,28 +1,15 @@
-const { defineConfig } = require('cypress');
-const { devServer } = require('@cypress/vite-dev-server');
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    specPattern: 'tests/e2e/**/*.e2e.js',
-    supportFile: 'tests/cypress/support/commands.js',
-    chromeWebSecurity: false,
-    video: false,
-    defaultCommandTimeout: 8000,
-    retries: {
-      runMode: 1,
-      openMode: 0,
-    },
     setupNodeEvents(on, config) {
-      on('dev-server:start', (options) => {
-        return devServer({
-          ...options,
-          framework: 'react',
-          viteConfig: require('../../../vite.config.js'), // Adjust if vite.config.js is elsewhere
-        });
-      });
-
-      return config;
+      require('@cypress/code-coverage/task')(on, config)
+      return config
     },
+    specPattern: 'tests/e2e/**/*.js',
+    supportFile: 'tests/cypress/support/e2e.js'
   },
-});
+  video: false,
+  screenshotOnRunFailure: true,
+})
