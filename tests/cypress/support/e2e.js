83dce14f1d240b02968cb6cfa908de4d5adfe812
Gourab Pal <110736631+gourabpal04@users.noreply.github.com>
2025-04-07 19:46:34 +0530

    Update e2e.js

--- tests/cypress/support/e2e.js
+++ tests/cypress/support/e2e.js
@@ -1,10 +1,6 @@
-// Import custom commands
 import './commands'
-
-// Optional: Setup code coverage
 import '@cypress/code-coverage/support'
 
-// You can also hook into test lifecycle events here
 beforeEach(() => {
   cy.log('Starting test...')
 })
